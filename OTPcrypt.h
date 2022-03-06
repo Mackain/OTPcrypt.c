4bd4e4dac77d7486c85dd55100477a6452909d41
Mackain <markus.lindgren@protonmail.com>
2022-03-06 20:54:47 +0100

    h file cleaned

--- OTPcrypt.h
+++ OTPcrypt.h
@@ -1,16 +1,17 @@
 #pragma once
-
+#include <stdio.h>
+#include <string.h>
 
 /*functions can be void cuz pointers lol*/
 
-void encrypt(char* encryptionKey, char* input);
+/*void encrypt(char* encryptionKey, char* input);
 
-void decrypt(char* decryptionKey, char* input, char* salt);
+void decrypt(char* decryptionKey, char* input, char* salt);*/
 
 void addSalt(char* unsaltedString);
 
-void removeSalt(char* saltedString);
+/*void removeSalt(char* saltedString);
 
 void generatePseudoRandomKey(char* key);
 
-void generateKeyFromSeed(char* key, char* seed);
+void generateKeyFromSeed(char* key, char* seed);*/
