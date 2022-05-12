18c164a9bba7e7bdb82b22033e6c168317295673
Mackain <markus.j.lindgren@gmail.com>
2022-05-12 22:53:44 +0200

    friendship ended with sizeof. now strlen is my best friend

--- OTPcrypt.c
+++ OTPcrypt.c
@@ -7,15 +7,18 @@
 const char alpha[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 1234567890\0";
 const int numberOfLetters = 63;
 
-void encrypt(char* encryptionKey, char* input) {
+void encrypt(char* encryptionKey, char* input)
+{
 
 }
 
-void decrypt(char* decryptionKey, char* input, char* salt) {
+void decrypt(char* decryptionKey, char* input, char* salt)
+{
 
 }
 
-int otpRand() {
+int otpRand()
+{
 	// use this to generate random numbers.
 	// it is not perfect, but this way we only have to refactor in one place in the future :)
 
@@ -30,7 +33,8 @@ int otpRand() {
 	return r;
 }
 
-void addSalt(char* unsaltedString) {
+void addSalt(char* unsaltedString)
+{
 	// initialize random thingy.
 	//srand(time(NULL));
 
@@ -62,33 +66,40 @@ void addSalt(char* unsaltedString) {
 	strcpy(unsaltedString, saltedString);
 }
 
-void removeSalt(char* saltedString) {
+void removeSalt(char* saltedString)
+{
+	printf("trololo\n");
 	// time too remove some salt (thats a lot of sodium)
 	// find char c at the end of the string.
 	char c = saltedString[(strlen(saltedString)-1)];
-	char* orgStrCpy = (char*) malloc(sizeof saltedString * sizeof(char));
+	char* orgStrCpy = malloc(strlen(saltedString) * sizeof(char));
 	strcpy(orgStrCpy, saltedString);
 
 	printf("THE KEY IS %c\n", c);
 	// calculate n, the index of C in the alphabet string.
-	int i;
-	char* e;
-	e = strchr(alpha, c);
+	int n = 0;
+	for (n; n < numberOfLetters; n++)
+	{
+		if(alpha[n] == c)
+		{
+			break;
+		}
+	}
 
-	printf("\n%s\n", e);
-
-	i = e - alpha;
-
-	printf("\n%i\n", i);
+	//int i;
+	//char* e;
+	//e = strchr(alpha, c);
+	//printf("\n%s\n", e);
+	//i = e - alpha;
+	printf("\n%i\n", n);
 
 	// remove n number of characters at the start of the input
-
-	char* unsaltedString = (char*) malloc(sizeof saltedString * sizeof(char));
-	strcpy( unsaltedString, &saltedString[i]);
-	//printf("\n%s\n", &saltedString[i]);
-	printf("\n%s\n", unsaltedString);
+	///printf("hahaha the size is %i", (strlen(orgStrCpy)));
+	char* unsaltedString = (char*) malloc(strlen(saltedString) * sizeof(char));
+	strncpy(unsaltedString, saltedString, strlen(saltedString)-1);
+	//printf("\n%s\n", &saltedString[n]);
 	// remove c from the end of the string.
-
+	strcpy(saltedString, &unsaltedString[n]);
 	// unsalted output may get a lenght of 0 if an invalid c is used... impostor detected!
 	// if so, just return the garbled input and pretend like it works as intended...
 	if(saltedString[0] == '\0')
--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "OTPcrypt.h"
 #include <stdio.h>
 
-int main(){
+int main()
+{
 	// this string must always have enough memory for its lenght + the alphabetstrings lenght +1
 	char str[100] = "old value";
 	addSalt(str);
-	//printf("lol salt was added: %s\n", str);
+	printf("lol salt was added: %s\n", str);
 	removeSalt(str);
+	printf("lol salt was removed: %s\n", str);
 	return 0;
 }
