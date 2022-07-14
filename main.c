8af5875ad53d09988531520d8f1f9b9efa2ab1e4
Mackain <markus.j.lindgren@gmail.com>
2022-07-14 22:38:29 +0200

    doesnt work but doenst matter

--- main.c
+++ main.c
@@ -3,27 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
+
+	// if arg1 == -e ? encryp : decrypt
+
+
 	// this string must always have enough memory for its lenght + the alphabetstrings lenght +1
 	char* str = " BABUCHKA YAYA ";
-
-	//str = addSalt(str);
-	//fetPrint(str);
-
-	//str = removeSalt(str);
-	//fetPrint(str);
-	//
-	str = encrypt("BABUCHKA", str);
+	str = decrypt("BABUCHKA", str);
 	printf("A\n");
-	char* str2 = (char*) malloc(strlen(str) * sizeof(char));
-	sprintf(str2, "%s", str);
-	str2 = decrypt("BABUCHKA", str2);
-	printf("B\n");
-	fetPrint(str2);
-	//printf("encrypted and salted: %s\n", str);
-	//printf("lol");
-	//decrypt("BABUCHKA", str);
-	//printf("decrypted (still salted): %s\n", str);
+
+
+
+	//char* str2 = (char*) malloc(strlen(str) * sizeof(char));
+	//sprintf(str2, "%s", str);
+	//str2 = decrypt("BABUCHKA", str2);
+	//printf("B\n");
+	//fetPrint(str2);
+
 	return 0;
 }
