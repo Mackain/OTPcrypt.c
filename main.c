46a0929a4e849a82dbd682f4100647e4c0c54cbc
JonasOlsson90 <olssonjonas@hotmail.com>
2022-08-03 07:54:21 +0200

    Added -h and --help for help

--- main.c
+++ main.c
@@ -7,14 +7,26 @@ int main(int argc, char **argv)
 {
 
 	// if arg1 == -e ? encryp : decrypt
+	if (strcmp(argv[1], "-h" ) == 0 || strcmp(argv[1], "--help") == 0)
+	{
+		printf("Enter flag followed by key then message.\n");
+		printf("-e		encrypt\n");
+		printf("-d		decrypt\n");
+		printf("-h		help\n");
+		printf("--help		help\n");
+		return 0;
+	}
+	
 	if (argc < 4)
 	{
 		printf("Too few arguments\n");
+		printf("Use -h or --help for instructions");
 		return 1;
 	}
 	if (argc > 4)
 	{
 		printf("Too many arguments\n");
+		printf("Use -h or --help for instructions");
 		return 1;
 	}
 	if (strcmp(argv[1], "-e") == 0)
@@ -29,6 +41,7 @@ int main(int argc, char **argv)
 	else
 	{
 		printf("Invalid syntax\n");
+		printf("Use -h or --help for instructions");
 		return 1;
 	}
 
